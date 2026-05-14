cbc29822e0c9ebd33c16649ac32e1058dc703bfe
Priscillaj65 <pj65@nmsu.edu>
2026-05-14 13:27:26 -0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -12,3 +12,18 @@ function searchServices() {
     }
   });
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("contactForm");
+  const message = document.getElementById("formMessage");
+
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      message.textContent = "✅ Thank you! Your message has been sent successfully.";
+      message.style.color = "green";
+
+      form.reset();
+    });
+  }
+});
